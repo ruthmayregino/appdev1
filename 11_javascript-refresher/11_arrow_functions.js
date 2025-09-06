3a0c45b075136102ea33cd9de0b975522ef3a509
Ruth May Regino <ruthmayregino@Ruths-MacBook-Air.local>
2025-09-06 08:48:01 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,12 @@
+const greet = (name) => {
+  return "Hello, " + name;
+};
+console.log(greet("Ruth"));
+
+const square = (n) => n * n;
+console.log("Square of 5:", square(5));
+
+const sayHi = () => {
+  console.log("Hi!");
+};
+sayHi();
