8b782aaa5aace026a005d477b1e1f14b88581de2
Ruth May Regino <ruthmayregino@Ruths-MacBook-Air.local>
2025-09-06 08:42:53 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,16 @@
+let name = "Ruth";
+let age = 21;
+let isStudent = true;
+
+console.log(name, typeof name);
+console.log(age, typeof age);
+console.log(isStudent, typeof isStudent);
+
+let a = 10, b = 5;
+console.log("Add:", a + b);
+console.log("Subtract:", a - b);
+console.log("Multiply:", a * b);
+console.log("Divide:", a / b);
+
+console.log("5 == 5:", "5" == 5);   
+console.log("5 === 5:", "5" === 5); 
