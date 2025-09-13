e867e2f03cf165fe6776aae35c78ae47d15f0ee0
Ruth May Regino <ruthmayregino@Ruths-MacBook-Air.local>
2025-09-13 14:32:22 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,12 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
-
 function AboutPage() {
   return (
     <>
@@ -18,19 +12,9 @@ function AboutPage() {
   );
 }
 
-
-
-
-
-
-
 function App() {
   return (
     <> 
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
     </>
   )
 }
