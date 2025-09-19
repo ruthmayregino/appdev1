bd58f0fb9296d846b09ab717375a213b546d6f13
Ruth May Regino <ruthmayregino@Ruths-MacBook-Air.local>
2025-09-20 05:07:55 +0800

    Done sharing data between components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-function MyButton() {
+export default function MyApp() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
@@ -12,19 +11,19 @@ function MyButton() {
   }
 
   return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
   );
 }
 
-export default function MyApp() {
+function MyButton({ count, onClick }) {
   return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
   );
 }
 
