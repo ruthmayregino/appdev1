c5e37de1879fb3ce021d7877f9a8fcdb3ad80764
Ruth May Regino <ruthmayregino@Ruths-MacBook-Air.local>
2025-09-20 05:06:13 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -4,16 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function App() {
-function handleClick() {
-    alert('You clicked me!');
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
   }
 
   return (
     <button onClick={handleClick}>
-      Click me
+      Clicked {count} times
     </button>
-  )
+  );
 }
 
-export default App
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
