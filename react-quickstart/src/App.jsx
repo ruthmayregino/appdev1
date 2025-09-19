f12bf89bab7a30b361912af988c436fd30c5b784
Ruth May Regino <ruthmayregino@Ruths-MacBook-Air.local>
2025-09-20 04:35:18 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,11 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-<img className="avatar" />
 
 function App() {
+  const user = {
+  name: 'RuthMay',
+  imageUrl: 'https://www.pinterest.com/pin/essentials-are-essential--8655424280926299/',
+  imageSize: 90,
+};
+
   return (
     <> 
+    <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+
     </>
   )
 }
