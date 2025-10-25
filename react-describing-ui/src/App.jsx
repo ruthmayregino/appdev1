54311af699a5e392139dbd54868b7bb789cc278b
ruthmayregino <ruthmayreginos2786@gmail.com>
2025-10-26 00:13:23 +0800

    Done with why understanding my UI as trees is useful

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -2,19 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { people } from './Data.js';
-import { getImageUrl } from './Utils.js';
+import RootComponent from './RootComponent.jsx';
 
-function Cup({ guest }) {
-  return <h2>Tea cup for guest #{guest}</h2>;
-}
-
-export default function TeaSet() {
-  return (
-    <>
-      <Cup guest={1} />
-      <Cup guest={2} />
-      <Cup guest={3} />
-    </>
-  );
+export default function App() {
+  return <RootComponent />;
 }
