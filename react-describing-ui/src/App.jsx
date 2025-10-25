cb7bb5c3b5ba45a2129b25cf43a09d6a403de154
ruthmayregino <ruthmayreginos2786@gmail.com>
2025-10-26 00:05:26 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -2,26 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Gallery from './Gallery'
 
-function Profile() {
+export default function TodoList() {
   return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
   );
 }
-
-function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
-}
-
-
-export default App
